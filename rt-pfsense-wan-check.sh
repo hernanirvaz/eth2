fb25e64c9d3414764d9acded2a1ddf905858c0c2
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-10-10 22:50:43 +0100

    cleanup

--- rt-pfsense-wan-check.sh
+++ rt-pfsense-wan-check.sh
@@ -4,8 +4,8 @@ WAN="re1"
 PGT="8.8.8.8"
 LOG="/root/rt-pfsense-wan-check.log"
 
-lgm() { echo "$(date '+%F %T') $1" >> "$LOG"; }
-pig() { /sbin/ping -c 3 -W 1 "$PGT" > /dev/null 2>&1; }
+lgm() { echo "$(date +'%Y-%m-%d %H:%M:%S') $1" >> $LOG; }
+pig() { /sbin/ping -c 3 -W 1 $PGT > /dev/null 2>&1; }
 
 if pig; then exit 0;fi
 /sbin/dhclient -r $WAN  ;sleep 5;/sbin/dhclient $WAN   ;sleep 20;if pig;then lgm "WAN recovered after DHCP renewal.";exit 0;fi
