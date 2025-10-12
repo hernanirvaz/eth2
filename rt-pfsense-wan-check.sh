e47f85cd9a709a75a5ba0dd9d1b57740e2f9a07a
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-10-12 09:47:47 +0100

    cleanup

--- rt-pfsense-wan-check.sh
+++ rt-pfsense-wan-check.sh
@@ -10,4 +10,4 @@ pig() { /sbin/ping -c 3 -W 1 $PGT > /dev/null 2>&1; }
 if pig; then exit 0;fi
 /sbin/dhclient -r $WAN  ;sleep 5;/sbin/dhclient $WAN   ;sleep 20;if pig;then lgm "WAN recovered after DHCP renewal.";exit 0;fi
 /sbin/ifconfig $WAN down;sleep 5;/sbin/ifconfig $WAN up;sleep 30;if pig;then lgm "WAN recovered after bounce."      ;exit 0;fi
-lgm "Performing pfSense reboot as last resort.";/sbin/reboot
+lgm "Performing pfSense reboot as last resort.";/etc/rc.reboot
