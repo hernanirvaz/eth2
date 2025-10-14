414c7d2c77a9489239edb047e1c03bbd1aa410f9
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-10-14 20:21:30 +0100

    cleanup

--- rt-pfsense-wan-check.sh
+++ rt-pfsense-wan-check.sh
@@ -11,10 +11,10 @@ LCK="/root/rt-pfsense-wan-check.lock"
 lgm() { echo "$(date +'%Y-%m-%d %H:%M:%S') $1" >> $LOG 2>&1; }
 pig() { /sbin/ping -c 5 -W 2 $1 > /dev/null 2>&1;local e=$?;[ $e -ne 0 ] && lgm "Server $1 unreachable (ping exit status: $e).";return $e; }
 pit() { pig $PG1 || pig $PG2 || pig $PG3;return $?; }
-dhr() { /sbin/dhclient -r $WAN   > /dev/null 2>&1 || lgm "Failed release DHCP $WAN." ;return $?; }
-dhn() { /sbin/dhclient    $WAN   > /dev/null 2>&1 || lgm "Failed renew DHCP $WAN."   ;return $?; }
-ifd() { /sbin/ifconfig $WAN down > /dev/null 2>&1 || lgm "Failed to bring $WAN down.";return $?; }  
-ifu() { /sbin/ifconfig $WAN up   > /dev/null 2>&1 || lgm "Failed to bring $WAN up."  ;return $?; }  
+dhr() { /sbin/dhclient -r $WAN      > /dev/null 2>&1 || lgm "Failed release DHCP $WAN." ;return $?; }
+dhn() { /sbin/dhclient    $WAN      > /dev/null 2>&1 || lgm "Failed renew DHCP $WAN."   ;return $?; }
+ifd() { /sbin/ifconfig    $WAN down > /dev/null 2>&1 || lgm "Failed to bring $WAN down.";return $?; }  
+ifu() { /sbin/ifconfig    $WAN up   > /dev/null 2>&1 || lgm "Failed to bring $WAN up."  ;return $?; }  
 
 if [ -f $LCK ];then PID=$(cat $LCK);if ps -p $PID > /dev/null 2>&1;then lgm "Another instance is running ($PID). Exiting.";exit 0;else rm -f $LCK;fi;fi;echo $$ > $LCK;trap "rm -f $LCK" EXIT
 
