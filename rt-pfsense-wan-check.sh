acaf94e504696b91f4e1f8a17527ae94b8ab14cb
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-11-24 17:09:04 +0000

    cleanup

--- rt-pfsense-wan-check.sh
+++ rt-pfsense-wan-check.sh
@@ -8,13 +8,13 @@ DNS="8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1 9.9.9.9 149.112.112.112 208.67.222.222 208.
 
 grn() { set -- $DNS;n=$(date +%s%N);r=$(awk -v m=1 -v x=$# -v s=$n "BEGIN{srand(s);print int(m+rand()*(x-m+1))}");c=1;for s in $DNS;do if [ $c -eq $r ];then echo $s;return;fi;c=$((c+1));done;echo 8.8.8.8; }
 lgm() { echo "$(date +'%Y-%m-%d %H:%M:%S') $1" >> $FIL.log 2>&1; }
-pig() { ping -c $2 -W 2 $1         > /dev/null 2>&1;e=$?;[ $e -ne 0 ] && lgm "ping -c$2 -W2 $1 erro ($e)";return $e; }
+pig() { ping -c $2 -W 4 $1         > /dev/null 2>&1;e=$?;[ $e -ne 0 ] && lgm "ping -c$2 -W2 $1 erro ($e)";return $e; }
 dhr() { dhclient -r $WAN           > /dev/null 2>&1;f=$?;[ $f -ne 0 ] && lgm "Failed release DHCP $WAN." ;return $f; }
 dhn() { dhclient    $WAN           > /dev/null 2>&1;g=$?;[ $g -ne 0 ] && lgm "Failed renew DHCP $WAN."   ;return $g; }
 ifd() { ifconfig    $WAN down      > /dev/null 2>&1;h=$?;[ $h -ne 0 ] && lgm "Failed to bring $WAN down.";return $h; }  
 ifu() { ifconfig    $WAN up        > /dev/null 2>&1;i=$?;[ $i -ne 0 ] && lgm "Failed to bring $WAN up."  ;return $i; }
 rex() { lgm "$1";/etc/rc.reboot    > /dev/null 2>&1;exit 1; }  
-pit() { pig $(grn) 3 || pig $(grn) 4; }
+pit() { pig $(grn) 4 || pig $(grn) 5; }
 
 if [ -f $FIL.lck ];then
     PID=$(cat $FIL.lck);rm -f $FIL.lck
