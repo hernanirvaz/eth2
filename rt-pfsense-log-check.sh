2d8e85eadce224cad1fbc5d6daa0783f78c21cca
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-12-26 23:30:46 +0000

    cleanup

--- rt-pfsense-log-check.sh
+++ rt-pfsense-log-check.sh
@@ -7,10 +7,14 @@ last reboot|head -n5|sed "s%  *% %g"|sort -k5n
 echo wan check log
 tail -n15 /root/wan-check.log
 
-i=${1:-0}
-echo $(date -v -${i}H '+%b %e %H'):??:?? init grep system.log
+case "$1" in
+    *[!0-9]*) f="$1"    ;i=${2:-0};g=''               ;;
+    *)        f="system";i=${1:-0};g='index.php|sshd' ;;
+esac
+
+echo $(date -v -${i}H '+%b %e %H'):??:?? init grep $f log
 while [ "$i" -ge 0 ]; do
-    grep "$(date -v -${i}H '+%b %e %H')" /var/log/system.log | grep -Ev 'index.php|sshd'
+    grep "$(date -v -${i}H '+%b %e %H')" "/var/log/$f.log" | if [ -z "$g" ]; then cat;else grep -Ev "$g";fi
     i=$((i - 1))
 done
 
