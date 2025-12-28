c077914d03f6d4017ab80df15b8b187b43b2d968
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-12-28 15:43:34 +0000

    cleanup

--- rt-pfsense-log-check.sh
+++ rt-pfsense-log-check.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # pfSense v2.8.1-RELEASE (amd64) FreeBSD 15.0-CURRENT
 
-echo last reboot
-last reboot|head -n5|sed "s%  *% %g"|sort -k5n
-
-echo wan check log
-tail -n15 /root/wan-check.log
+echo pfSense uptime:;uptime;echo
+echo pfSense wan check log;tail -n15 /root/wan-check.log
 
 case "$1" in
     *[!0-9]*) f="$1"    ;i=${2:-0};g=''               ;;
@@ -16,7 +13,6 @@ echo $(date -v -${i}H '+%b %e %H'):??:?? init grep $f log
 while [ "$i" -ge 0 ]; do
     grep "$(date -v -${i}H '+%b %e %H')" "/var/log/$f.log" | if [ -z "$g" ]; then cat;else grep -Ev "$g";fi
     i=$((i - 1))
-done
+done;echo
 
-echo
 date +"%Y-%m-%d %H:%M:%S"
