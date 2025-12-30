bd0a2ca9b9e5d5037f87f907b676d8960bdded0a
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2025-12-30 07:56:58 +0000

    cleanup

--- rt-pfsense-log-check.sh
+++ rt-pfsense-log-check.sh
@@ -5,7 +5,7 @@
 case "$1" in
     *[!0-9]*) 
         f="$1"    ;i=${2:-0};g='' ;;               
-    *)  f="system";i=${1:-0};g='Successful +login|sshd' 
+    *)  f="system";i=${1:-0};g='logged|login|sshd' 
         echo pfSense uptime:      ;uptime;echo
         echo pfSense wan check log;tail -n15 /root/wan-check.log;echo
         ;;
@@ -13,7 +13,7 @@ esac
 
 echo $(date -v -${i}H '+%b %e %H'):??:?? init grep $f log
 while [ "$i" -ge 0 ]; do
-    grep "$(date -v -${i}H '+%b %e %H')" "/var/log/$f.log" | if [ -z "$g" ]; then cat;else grep -Ev "$g";fi
+    grep "$(date -v -${i}H '+%b %e %H')" /var/log/$f.log* | if [ -z "$g" ]; then cat;else grep -Ev "$g";fi
     i=$((i - 1))
 done;echo
 
