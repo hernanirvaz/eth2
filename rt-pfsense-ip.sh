d65614e92b7352bdb880933ccedf12ef17814298
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2026-01-17 09:03:49 +0000

    cleanup

---
+++ rt-pfsense-ip.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# pfSense v2.8.1-RELEASE (amd64) FreeBSD 15.0-CURRENT
+
+ift() { ifconfig igc0.12 2>/dev/null | grep -q "status: active"; }
+ifw() { if ift;then echo igc0.12;else echo igc0;fi; }
+
+if   [ $1 = ip4 ];then ifconfig $(ifw)|grep inet |grep -v inet6|awk '{print $2}'
+elif [ $1 = ip6 ];then ifconfig igc1  |grep inet6|grep -v fe80 |awk '{print $2}'
+elif [ $1 = ipc ];then ifconfig igc0;ift && ifconfig igc0.12;ifconfig igc1
+elif [ $1 = ipr ];then /usr/bin/netstat -rn
+elif [ $1 = ipd ];then ndp -an
+else echo "Usage: rt-pfsense-ip.sh {ip4|ip6|ipc|ipr|ipd}"
+fi
