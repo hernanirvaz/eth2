7d5f4de86f041b81586e8614b28e7ceefc100bcc
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2026-07-20 07:10:48 +0100

    cleanup

--- rt-pfsense-ip.sh
+++ rt-pfsense-ip.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # pfSense v2.8.1-RELEASE (amd64) FreeBSD 15.0-CURRENT
 
-ifo() { echo ifconfig $1;ifconfig $1;sleep 1;echo; }
-ift() { ifconfig igc0.12 2>/dev/null | grep -q "status: active"; }
-ifw() { if ift;then echo igc0.12;else echo igc0;fi; }
+CFG='/conf/config.xml'
+[ -f $CFG  ] || exit 1;[ -d /root ] || exit 1
+DNL='/dev/null'
+WAN=$(awk -F'[<>]' '/<wan>/{in_wan=1} in_wan && /<if>/{print $3; exit}' $CFG 2>$DNL)
 
-if   [ $1 = ip4 ];then ifconfig $(ifw)|grep inet |grep -v inet6|awk '{print $2}'
+ifo() { echo ifconfig $1;ifconfig $1;sleep 1;echo; }
+
+if   [ $1 = ip4 ];then ifconfig $WAN  |grep inet |grep -v inet6|awk '{print $2}'
 elif [ $1 = ip6 ];then ifconfig igc1  |grep inet6|grep -v fe80 |awk '{print $2}'
 elif [ $1 = ipc ];then ifo igc0;ifo igc0.12;ifo igc1
 elif [ $1 = ipr ];then /usr/bin/netstat -rn
