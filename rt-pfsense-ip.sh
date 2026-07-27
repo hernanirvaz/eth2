a825d13528b17bf40775208da7c80ed17483278f
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2026-07-28 00:29:50 +0100

    cleanup

--- rt-pfsense-ip.sh
+++ rt-pfsense-ip.sh
@@ -10,7 +10,7 @@ ifo() { echo ifconfig $1;ifconfig $1;sleep 1;echo; }
 
 if   [ $1 = ip4 ];then ifconfig $WAN  |grep inet |grep -v inet6|awk '{print $2}'
 elif [ $1 = ip6 ];then ifconfig igc1  |grep inet6|grep -v fe80 |awk '{print $2}'
-elif [ $1 = ipc ];then ifo igc0;ifo igc0.12;ifo igc1
+elif [ $1 = ipc ];then ifo igc0;ifo igc0.12;ifo igc1;ifo igc2
 elif [ $1 = ipr ];then /usr/bin/netstat -rn
 elif [ $1 = ipd ];then ndp -an
 else echo "Usage: rt-pfsense-ip.sh {ip4|ip6|ipc|ipr|ipd}"
