1d80f72326fad9db3aa4b97678b05fd2c417216c
Hernani Rodrigues Vaz <hernanirvaz@gmail.com>
2026-08-15 12:47:10 +0100

    cleanup

--- rt-pfsense-ip.sh
+++ rt-pfsense-ip.sh
@@ -7,11 +7,13 @@ DNL='/dev/null'
 WAN=$(awk -F'[<>]' '/<wan>/{in_wan=1} in_wan && /<if>/{print $3; exit}' $CFG 2>$DNL)
 
 ifo() { ifconfig $1|sed -E 's/^[ \t]+/  * /;s/^i/* i/';sleep 1; }
+nst() { netstat -rn | grep -Ev 'Routing|^fe80' | sed -E 's%^([^ID])%  * \1%;s%^([ID])%* \1%';sleep 1; }
 
 if   [ $1 = ip4 ];then ifconfig $WAN  |grep inet |grep -v inet6|awk '{print $2}'
 elif [ $1 = ip6 ];then ifconfig igc1  |grep inet6|grep -v fe80 |awk '{print $2}'
 elif [ $1 = ipc ];then ifo igc0;ifo igc0.12;ifo igc1;ifo igc2
-elif [ $1 = ipr ];then /usr/bin/netstat -rn
+elif [ $1 = ipr ];then nst
+elif [ $1 = ipa ];then ifo igc0;ifo igc0.12;ifo igc1;ifo igc2;nst
 elif [ $1 = ipd ];then ndp -an
 else echo "Usage: rt-pfsense-ip.sh {ip4|ip6|ipc|ipr|ipd}"
 fi
